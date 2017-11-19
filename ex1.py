e9283e827d235e81205f4a2886aeb6376ca1ceaa
Erin <erin@Macbook-Pro.local>
2017-11-19 14:06:26 -0500

    ex1.py

---
+++ ex1.py
@@ -0,0 +1,8 @@
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun")
+print("Yay! Printing.")
+print("I'd much rather you not")
+# This is a comment
+print('I "said" do not touch this.')
